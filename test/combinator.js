332eb2c5f8acd5a3b9cc808bd26e997b84a6e208
Susisu <susisu2413@yahoo.co.jp>
2014-10-30 21:07:27 +0900

    Add test for 'optionMaybe'

--- test/combinator.js
+++ test/combinator.js
@@ -363,7 +363,120 @@ describe("combinator", function () {
     });
 
     describe("optionMaybe(parser)", function () {
+        it("should try 'parser' and return an array containing only the result when 'parser' succeeded or failed with consumption", function () {
+            lq.combinator.optionMaybe(
+                alwaysCSuc(
+                    "foo",
+                    new State("def", new SourcePos("test", 1, 2), "none"),
+                    new ParseError(
+                        new SourcePos("test", 1, 2),
+                        [new ErrorMessage(ErrorMessageType.MESSAGE, "csuc")]
+                    )
+                )
+            ).run(
+                new State("abc", SourcePos.init("test"), "some"),
+                function (value, state, error) {
+                    lq.util.ArrayUtil.equals(value, ["foo"]);
+                    State.equals(
+                        state,
+                        new State("def", new SourcePos("test", 1, 2), "none")
+                    ).should.be.ok;
+                    ParseError.equals(
+                        error,
+                        new ParseError(
+                            new SourcePos("test", 1, 2),
+                            [new ErrorMessage(ErrorMessageType.MESSAGE, "csuc")]
+                        )
+                    ).should.be.ok;
+                },
+                throwError,
+                throwError,
+                throwError
+            );
 
+            lq.combinator.optionMaybe(
+                alwaysCErr(
+                    new ParseError(
+                        new SourcePos("test", 1, 2),
+                        [new ErrorMessage(ErrorMessageType.MESSAGE, "cerr")]
+                    )
+                )
+            ).run(
+                new State("abc", SourcePos.init("test"), "some"),
+                throwError,
+                function (error) {
+                    ParseError.equals(
+                        error,
+                        new ParseError(
+                            new SourcePos("test", 1, 2),
+                            [new ErrorMessage(ErrorMessageType.MESSAGE, "cerr")]
+                        )
+                    ).should.be.ok;
+                },
+                throwError,
+                throwError
+            );
+
+            lq.combinator.optionMaybe(
+                alwaysESuc(
+                    "foo",
+                    new State("def", new SourcePos("test", 1, 2), "none"),
+                    new ParseError(
+                        new SourcePos("test", 1, 2),
+                        [new ErrorMessage(ErrorMessageType.MESSAGE, "esuc")]
+                    )
+                )
+            ).run(
+                new State("abc", SourcePos.init("test"), "some"),
+                throwError,
+                throwError,
+                function (value, state, error) {
+                    lq.util.ArrayUtil.equals(value, ["foo"]);
+                    State.equals(
+                        state,
+                        new State("def", new SourcePos("test", 1, 2), "none")
+                    ).should.be.ok;
+                    ParseError.equals(
+                        error,
+                        new ParseError(
+                            new SourcePos("test", 1, 2),
+                            [new ErrorMessage(ErrorMessageType.MESSAGE, "esuc")]
+                        )
+                    ).should.be.ok;
+                },
+                throwError
+            );
+        });
+
+        it("should try 'parser' and succeed with an empty array when 'parser' failed without consumption", function () {
+            lq.combinator.optionMaybe(
+                alwaysEErr(
+                    new ParseError(
+                        new SourcePos("test", 1, 2),
+                        [new ErrorMessage(ErrorMessageType.MESSAGE, "eerr")]
+                    )
+                )
+            ).run(
+                new State("abc", SourcePos.init("test"), "some"),
+                throwError,
+                throwError,
+                function (value, state, error) {
+                    lq.util.ArrayUtil.equals(value, []);
+                    State.equals(
+                        state,
+                        new State("abc", SourcePos.init("test"), "some")
+                    ).should.be.ok;
+                    ParseError.equals(
+                        error,
+                        new ParseError(
+                            new SourcePos("test", 1, 2),
+                            [new ErrorMessage(ErrorMessageType.MESSAGE, "eerr")]
+                        )
+                    ).should.be.ok;
+                },
+                throwError
+            );
+        });
     });
 
     describe("optional(parser)", function () {
