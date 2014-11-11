f773584a5b83e7c004c039253156232792a2e4b2
Susisu <susisu2413@yahoo.co.jp>
2014-11-12 00:34:00 +0900

    Add partial tests for 'buildExpressionParser'
    for unary operators

--- test/expr.js
+++ test/expr.js
@@ -8,6 +8,7 @@ var should = require("should");
 var lq = Object.freeze({
     "error" : require("../lib/error"),
     "expr"  : require("../lib/expr"),
+    "char"  : require("../lib/char"),
     "pos"   : require("../lib/pos"),
     "prim"  : require("../lib/prim"),
     "util"  : require("../lib/util"),
@@ -73,6 +74,291 @@ describe("expr", function () {
                 operator.parser.should.equal(dummy);
                 operator.assoc.should.equal("assocNone");
             });
+        });
+    });
+
+    describe("buildExpressionParser(operators, term)", function () {
+        it("should create a parser to parse expressions", function () {
+            function Value (value) {
+                this.value = value;
+            }
+            Value.prototype.toString = function () {
+                return this.value;
+            };
+            function Prefix (op, expr) {
+                this.op = op;
+                this.expr = expr;
+            }
+            Prefix.prototype.toString = function () {
+                return "(" + this.op + this.expr.toString() + ")";
+            };
+            function Postfix (op, expr) {
+                this.op = op;
+                this.expr = expr;
+            }
+            Postfix.prototype.toString = function () {
+                return "(" + this.expr.toString() + this.op + ")";
+            };
+            function Infix (op, left, right) {
+                this.op = op;
+                this.left = left;
+                this.right = right;
+            }
+            Infix.prototype.toString = function () {
+                return "(" + this.left.toString() + this.op + this.right.toString() + ")";
+            };
+
+            var newValueM = lq.prim.fmap(function (value) { return new Value(value); });
+            var newPrefixOpM = lq.prim.fmap(function (op) {
+                return function (expr) { return new Prefix(op, expr); };
+            });
+            var newPostfixOpM = lq.prim.fmap(function (op) {
+                return function (expr) { return new Postfix(op, expr); };
+            });
+            var newInfixOpM = lq.prim.fmap(function (op) {
+                return function (left, right) { return new Infix(op, left, right); };
+            });
+
+            (function () {
+                var p = new lq.prim.Parser(function (state, csuc, cerr, esuc, eerr) {
+                    switch (state.input.charAt(0)) {
+                        case "a": return csuc(
+                                "a" + state.position.column.toString(),
+                                new State(
+                                    state.input.substr(1),
+                                    state.position.setColumn(state.position.column + 1),
+                                    "none"
+                                ),
+                                new ParseError(
+                                    state.position.setColumn(state.position.column + 1),
+                                    [new ErrorMessage(ErrorMessageType.MESSAGE, "csuc")]
+                                )
+                            );
+                        case "b": return cerr(
+                                new ParseError(
+                                    state.position.setColumn(state.position.column + 1),
+                                    [new ErrorMessage(ErrorMessageType.MESSAGE, "cerr")]
+                                )
+                            );
+                        case "c": return esuc(
+                                "c" + state.position.column.toString(),
+                                new State(
+                                    state.input.substr(1),
+                                    state.position,
+                                    "none"
+                                ),
+                                new ParseError(
+                                    state.position,
+                                    [new ErrorMessage(ErrorMessageType.MESSAGE, "esuc")]
+                                )
+                            );
+                        default: return eerr(
+                                new ParseError(
+                                    state.position,
+                                    [new ErrorMessage(ErrorMessageType.MESSAGE, "eerr")]
+                                )
+                            );
+                    }
+                });
+                var term = newValueM(p);
+                var parser = lq.expr.buildExpressionParser([], term);
+
+                parser.run(
+                    new State("aabcd", SourcePos.init("test"), "none"),
+                    function (value, state, error) {
+                        value.toString().should.equal("a1");
+                        State.equals(
+                            state,
+                            new State("abcd", new SourcePos("test", 1, 2), "none")
+                        ).should.be.ok;
+                        ParseError.equals(
+                            error,
+                            new ParseError(
+                                new SourcePos("test", 1, 2),
+                                [new ErrorMessage(ErrorMessageType.MESSAGE, "csuc")]
+                            )
+                        ).should.be.ok;
+                    },
+                    throwError,
+                    throwError,
+                    throwError
+                );
+
+                parser.run(
+                    new State("babcd", SourcePos.init("test"), "none"),
+                    throwError,
+                    function (error) {
+                        ParseError.equals(
+                            error,
+                            new ParseError(
+                                new SourcePos("test", 1, 2),
+                                [new ErrorMessage(ErrorMessageType.MESSAGE, "cerr")]
+                            )
+                        ).should.be.ok;
+                    },
+                    throwError,
+                    throwError
+                );
+
+                parser.run(
+                    new State("cabcd", SourcePos.init("test"), "none"),
+                    throwError,
+                    throwError,
+                    function (value, state, error) {
+                        value.toString().should.equal("c1");
+                        State.equals(
+                            state,
+                            new State("abcd", new SourcePos("test", 1, 1), "none")
+                        ).should.be.ok;
+                        ParseError.equals(
+                            error,
+                            new ParseError(
+                                new SourcePos("test", 1, 1),
+                                [new ErrorMessage(ErrorMessageType.MESSAGE, "esuc")]
+                            )
+                        ).should.be.ok;
+                    },
+                    throwError
+                );
+
+                parser.run(
+                    new State("dabcd", SourcePos.init("test"), "none"),
+                    throwError,
+                    throwError,
+                    throwError,
+                    function (error) {
+                        ParseError.equals(
+                            error,
+                            new ParseError(
+                                new SourcePos("test", 1, 1),
+                                [new ErrorMessage(ErrorMessageType.MESSAGE, "eerr")]
+                            )
+                        ).should.be.ok;
+                    }
+                );
+            })();
+
+            (function () {
+                var term = newValueM(lq.char.letter);
+                var preop = new Operator(
+                    OperatorType.PREFIX,
+                    newPrefixOpM(lq.char.char("+"))
+                );
+                var postop = new Operator(
+                    OperatorType.POSTFIX,
+                    newPostfixOpM(lq.char.char("-"))
+                );
+                var parser = lq.expr.buildExpressionParser([[preop, postop]], term);
+
+                parser.run(
+                    new State("a", SourcePos.init("test"), "none"),
+                    function (value, state, error) {
+                        value.toString().should.equal("a");
+                        State.equals(
+                            state,
+                            new State("", new SourcePos("test", 1, 2), "none")
+                        ).should.be.ok;
+                        ParseError.equals(
+                            error,
+                            new ParseError(
+                                new SourcePos("test", 1, 2),
+                                [
+                                    new ErrorMessage(ErrorMessageType.SYSTEM_UNEXPECT, ""),
+                                    new ErrorMessage(ErrorMessageType.EXPECT, "")
+                                ]
+                            )
+                        ).should.be.ok;
+                    },
+                    throwError,
+                    throwError,
+                    throwError
+                );
+                
+                parser.run(
+                    new State("+a", SourcePos.init("test"), "none"),
+                    function (value, state, error) {
+                        value.toString().should.equal("(+a)");
+                        State.equals(
+                            state,
+                            new State("", new SourcePos("test", 1, 3), "none")
+                        ).should.be.ok;
+                        ParseError.equals(
+                            error,
+                            new ParseError(
+                                new SourcePos("test", 1, 3),
+                                [
+                                    new ErrorMessage(ErrorMessageType.SYSTEM_UNEXPECT, ""),
+                                    new ErrorMessage(ErrorMessageType.EXPECT, "")
+                                ]
+                            )
+                        ).should.be.ok;
+                    },
+                    throwError,
+                    throwError,
+                    throwError
+                );
+
+                parser.run(
+                    new State("a-", SourcePos.init("test"), "none"),
+                    function (value, state, error) {
+                        value.toString().should.equal("(a-)");
+                        State.equals(
+                            state,
+                            new State("", new SourcePos("test", 1, 3), "none")
+                        ).should.be.ok;
+                        ParseError.equals(
+                            error,
+                            new ParseError(
+                                new SourcePos("test", 1, 3),
+                                []
+                            )
+                        ).should.be.ok;
+                    },
+                    throwError,
+                    throwError,
+                    throwError
+                );
+
+                parser.run(
+                    new State("+a-", SourcePos.init("test"), "none"),
+                    function (value, state, error) {
+                        value.toString().should.equal("((+a)-)");
+                        State.equals(
+                            state,
+                            new State("", new SourcePos("test", 1, 4), "none")
+                        ).should.be.ok;
+                        ParseError.equals(
+                            error,
+                            new ParseError(
+                                new SourcePos("test", 1, 4),
+                                []
+                            )
+                        ).should.be.ok;
+                    },
+                    throwError,
+                    throwError,
+                    throwError
+                );
+
+                parser.run(
+                    new State("+", SourcePos.init("test"), "none"),
+                    throwError,
+                    function (error) {
+                        ParseError.equals(
+                            error,
+                            new ParseError(
+                                new SourcePos("test", 1, 2),
+                                [
+                                    new ErrorMessage(ErrorMessageType.SYSTEM_UNEXPECT, ""),
+                                    new ErrorMessage(ErrorMessageType.EXPECT, "letter")
+                                ]
+                            )
+                        ).should.be.ok;
+                    },
+                    throwError,
+                    throwError
+                );
+            })();
         });
     });
 });
