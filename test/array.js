2a4713d790e686942c50af951069fbaffaf589cd
Susisu <susisu2413@yahoo.co.jp>
2015-01-01 14:13:34 +0900

    Change assertion style
    it should be expected

--- test/array.js
+++ test/array.js
@@ -4,7 +4,7 @@
  */
 
 var chai   = require("chai"),
-    should = chai.should();
+    expect = chai.expect;
 
 var lq = Object.freeze({
     "array": require("../lib/array"),
@@ -16,19 +16,15 @@ describe("array", function () {
     describe("Array", function () {
         describe("#uncons()", function () {
             it("should return an empty array when the array is empty", function () {
-                [].uncons().length.should.equal(0);
+                expect([].uncons()).to.be.empty;
             });
 
             it("should return an array, the first element of which is the head of the array and the secound is the rest", function () {
                 var u1 = [1].uncons();
-                u1.length.should.equal(2);
-                u1[0].should.equal(1);
-                lq.util.ArrayUtil.equals(u1[1], []).should.be.ok;
+                expect(u1).to.deep.equal([1, []]);
 
                 var u2 = [1, 2, 3, 4, 5].uncons();
-                u2.length.should.equal(2);
-                u2[0].should.equal(1);
-                lq.util.ArrayUtil.equals(u2[1], [2, 3, 4, 5]).should.be.ok;
+                expect(u2).to.deep.equal([1, [2, 3, 4, 5]]);
             });
         });
     });
--- test/string.js
+++ test/string.js
@@ -4,7 +4,7 @@
  */
 
 var chai   = require("chai"),
-    should = chai.should();
+    expect = chai.expect;
 
 var lq = Object.freeze({
     "string": require("../lib/string")
@@ -15,19 +15,15 @@ describe("string", function () {
     describe("String", function () {
         describe("#uncons()", function () {
             it("should return an empty array when the string is empty", function () {
-                "".uncons().length.should.equal(0);
+                expect("".uncons()).to.be.empty;
             });
 
             it("should return an array, the first element of which is the head of the string and the secound is the rest", function () {
                 var u1 = "a".uncons();
-                u1.length.should.equal(2);
-                u1[0].should.equal("a");
-                u1[1].should.equal("");
+                expect(u1).to.deep.equal(["a", ""]);
 
                 var u2 = "abcde".uncons();
-                u2.length.should.equal(2);
-                u2[0].should.equal("a");
-                u2[1].should.equal("bcde");
+                expect(u2).to.deep.equal(["a", "bcde"]);
             });
         });
     });
